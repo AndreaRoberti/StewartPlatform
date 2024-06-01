10d101a2422a3aeffbdf8e8a7f7104e6ff3e3129
corbe02 <147498479+corbe02@users.noreply.github.com>
2024-06-01 10:05:56 +0200

    Add files via upload

--- script/stewart_platform_ekf.py
+++ script/stewart_platform_ekf.py
@@ -7,6 +7,8 @@ import tf
 from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
 from filterpy.common import Saver
 import matplotlib.pyplot as plt
+import scipy.linalg as linalg
+from StewartPlatform import *
 
 
 class StewartPlatformEKF():
@@ -21,6 +23,7 @@ class StewartPlatformEKF():
 
         self.tf_listener_ = tf.TransformListener()
         self.br_ = tf.TransformBroadcaster()
+        self.start_t_ = rospy.get_time()
 
     def pose_callback(self,msg):
         self.sphere_pose_ = msg
@@ -46,29 +49,29 @@ class StewartPlatformEKF():
 
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             print('No transform available')
+        
+    def Hx(self, x):
+        """ takes a state variable and returns the measurement
+        that would correspond to that state.
+        ybar è la posizione che ricavo dalla simulazione/ mean position of the feature in 3d space
+        """
+        T_sphere_to_camera = self.get_transform('camera_link','red_sphere')
+        ybar = T_sphere_to_camera[:3,3]
+        alpha = x[0]
+        tau= x[1]
+        b = x[2]
+        z0 = x[3]
+        eig = np.array([1,1,1])
+        n=3
+        T_camera_to_world = self.get_transform('world', 'camera_link') #trasformation matrix camera world 
+        rW = T_camera_to_world[:3,3] #translation vector
+        Rrw = T_camera_to_world[:3,:3] #rotation matrixù
+        #print(Rrw.dot(eig))
+        p1 = Rrw.dot(eig)*(z0-b*(np.cos(alpha)**(2*n))) + ybar -rW
+        #print(np.shape(p1))
+        return p1
 
-    def update(self):
-        T_sphere_to_world = self.get_transform('world','red_sphere')
-                
-        def Hx(x,n):
-            """ takes a state variable and returns the measurement
-            that would correspond to that state.
-            ybar è la posizione che ricavo dalla simulazione/ mean position of the feature in 3d space
-            """
-            T_sphere_to_camera = self.get_transform('camera_link','red_sphere')
-            ybar = T_sphere_to_camera[:3,3]
-            alpha = x[0]
-            tau= x[1]
-            b = x[2]
-            z0 = x[3]
-            eig = [1,1,1]
-            T_camera_to_world = self.get_transform('world', 'camera_link') #trasformation matrix camera world 
-            rW = T_camera_to_world[:3,3] #translation vector
-            Rrw = T_camera_to_world[:2,:2] #rotation matrix
-            p1 = Rrw.dot(eig)*(z0-b*(np.cos(alpha)**(2*n))) + ybar - rW #Rrw è la predicted camera rotation e rW è la predicted camera position, sono da considerare nulli???? In quale sr lavoro
-            return p1[3] #solo la terza componente?
-
-        def Jacobian(m):
+    def Jacobian(self,m):
             """ compute Jacobian"""
             alpha = m[0]
             tau= m[1]
@@ -77,19 +80,26 @@ class StewartPlatformEKF():
             eig =[1,1,1]
             n = 3
             T_camera_to_world = self.get_transform('world', 'camera_link') #trasformation matrix camera world 
-            rW = T_camera_to_world[:2,3] #translation vector
-            Rrw = T_camera_to_world[:2,:2] #rotation matrix
+            rW = T_camera_to_world[:3,3] #translation vector
+            Rrw = T_camera_to_world[:3,:3] #rotation matrix
 
-        
+            
             dalpha = Rrw.dot(eig) * (n * b * np.sin(alpha) * (np.cos(alpha))**(n - 1))
             dtau = [0,0,0]
             db =-Rrw.dot(eig)*(np.cos(alpha)**n)
             dz0 = Rrw.dot(eig)
             
-            return np.transpose(np.array([dalpha,dtau,db,dz0]))
-
-        rk = ExtendedKalmanFilter(dim_x=4, dim_z=1)
-        t= rospy.get_time()#time
+            H = np.array([dalpha,dtau,db,dz0])
+            #print(H.T)
+            return H.T 
+            #return (np.array([dalpha,dtau,db,dz0]))   
+    
+    def update(self):
+        #T_sphere_to_world = self.get_transform('world','red_sphere')
+        #self.Hx(x,n)
+        rk = ExtendedKalmanFilter(dim_x=4, dim_z=3)
+        t= rospy.get_time() - self.start_t_ #time
+        #print(t)
         z0 = 0.95 #exale position
         b = 3.09 #amplitude 
         tau = 32.38 #frequency
@@ -98,16 +108,15 @@ class StewartPlatformEKF():
 
         #parametri vari
         rk.x = [alpha, tau, b, z0]
-        
         rk.F = np.eye(4) + np.array(([[0, 1, 0,0],[0, 0, 0, 0],[0, 0, 0, 0],[0,0,0,0]]))*t
-        Φ_tau = Φ_b = Φ_z0 = 1
-        rk.Q = ([[Φ_tau  * (t**3) / 3, Φ_tau * (t**2) / 2, 0, 0],
-                        [Φ_tau * (t**2) / 2, Φ_tau * t, 0, 0],
-                        [0, 0, Φ_b * t, 0],
-                        [0, 0, 0, Φ_z0 * t]])
-        rk.R = np.diag(
-            (np.random.normal(0,0.01,1))**2)#rumore bianco gaussiano (media,ampiezza,numero_elementi), in alternativa si può generale con filterpy
-        rk.P = np.eye(4)
+        #print(rk.F)
+        phi_tau = phi_b = phi_z0 = 1
+        rk.Q = ([[phi_tau  * (t**3) / 3, phi_tau * (t**2) / 2, 0, 0],
+                        [phi_tau * (t**2) / 2, phi_tau * t, 0, 0],
+                        [0, 0, phi_b * t, 0],
+                        [0, 0, 0, phi_z0 * t]])
+        rk.R = np.eye(3)*(np.random.normal(0,0.01,1)**2)#rumore bianco gaussiano (media,ampiezza,numero_elementi), in alternativa si può generale con filterpy
+        
 
         rs = [] #misurazioni simulate z
         xs = [] #tutti i valori di x
@@ -115,18 +124,29 @@ class StewartPlatformEKF():
         pos = [] #posizioni reali z
 
         s = Saver(rk)
-        for i in range(200): #????
+        for i in range(20): #????
+            #print(i)
             T_sphere_to_camera = self.get_transform('camera_link','red_sphere')
-            print(T_sphere_to_camera)
-            z = T_sphere_to_camera[2,3] #solo spostamento lungo la z
-            pos.append(z)
-
-            rk.update(z, Jacobian, Hx)
-            ps.append(rk.P)
-            rk.predict()
-            xs.append(rk.x)
-            rs.append(z)
-            s.save()
+            if isinstance(T_sphere_to_camera, np.ndarray):
+               # print(T_sphere_to_camera)
+                z = T_sphere_to_camera[:3,3] #tutto il vettore di traslazione
+                pos.append(z)
+                #print('--------------------------------')
+                #print(np.shape(np.subtract(z,self.Hx(rk.x))))
+               
+                #print('********************************')
+                #print(self.Jacobian(rk.x))
+                #print('-----------------------------')
+                #print(np.shape(z))
+                rk.update(z, self.Jacobian, self.Hx,R=rk.R)
+                #print('update concluso')
+                #print(rk.P)
+                #rk.predict_update(z,self.Jacobian,self.Hx)
+                ps.append(rk.P)
+                rk.predict()
+                xs.append(rk.x)
+                rs.append(z)
+                s.save()
 
         # vedi test mahalanobis, serve?
         # self.sphere_matrix_
